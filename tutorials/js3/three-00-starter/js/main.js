df4ca795a539e270a42145277256d11761ef1375
emilyrojasmartinez-coder <emilyrojasmartinez@gmail.com>
2025-11-07 00:12:04 -0800

    files

---
+++ tutorials/js3/three-00-starter/js/main.js
@@ -0,0 +1,80 @@
+// Basic Three.JS scene from documentation, importing Three.JS through a CDN 
+// https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
+
+
+//~~~~~~~Import Three.js (also linked to as import map in HTML)~~~~~~
+import * as THREE from 'three';
+
+// Import add-ons
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+
+
+
+
+
+let scene, camera, renderer, cube;
+function init(){
+
+ scene = new THREE.Scene();
+
+ const light = new THREE.DirectionalLight(0xfffff, 3);
+light.position.set(1,1,5);
+scene.add(light);
+
+ camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+renderer = new THREE.WebGLRenderer({ antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+
+// ~~~~~~~~~~~~~~~~ Initiate add-ons ~~~~~~~~~~~~~~~~
+const controls = new OrbitControls(camera, renderer.domElement);
+const loader = new GLTFLoader(); // to load 3d models
+
+loader.load('../assets/charmander_004_sv/scene.gltf', function (gltf){
+    const charmander = gltf.scene;
+    scene.add(charmander);
+})
+
+
+
+// ~~~~~~~~~~~~~~~~ Create scene here ~~~~~~~~~~~~~~~~
+// →→→→→→ Follow next steps in tutorial: 
+// // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
+
+
+
+//const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+//const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//const texture = new THREE.TextureLoader().load('textures/grasslight-big.jpg')
+//const material = new THREE.MeshBasicMaterial({ map: texture} );
+ //cube = new THREE.Mesh( geometry, material );
+//scene.add( cube );
+
+camera.position.z = 5;
+
+}
+// ~~~~~~~~~~~~~~~~Set up~~~~~~~~~~~~~~~~
+
+function animate(){
+    requestAnimationFrame(animate);
+
+   // cube.rotation.x += .010;
+       // s   cube.rotation.y += .010;
+    renderer.render(scene, camera);
+
+
+}
+function onWindowResize(){
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', onWindowResize, false)
+init();
+
+    animate();
