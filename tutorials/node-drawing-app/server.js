2867a19290951c7433ea336d6992389d6c8c0524
emilyrojasmartinez-coder <emilyrojasmartinez@gmail.com>
2025-11-13 23:23:45 -0800

    push

--- tutorials/node-drawing-app/server.js
+++ tutorials/node-drawing-app/server.js
@@ -1,1 +1,28 @@
-console.log("hello world!")
+
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+console.log("hello world!");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log('newConnection: ' + socket.id);
+  
+socket.on('mouse', mouseMsg);
+
+function mouseMsg(data){
+    socket.broadcast.emit('mouse', data);
+    console.log(data);
+}
+
+
+}
